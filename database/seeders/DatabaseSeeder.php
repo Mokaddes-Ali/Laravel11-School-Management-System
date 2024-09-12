55f20b3f562f5a60ccee3d0c83164a9bfc3dfc66
mokadde <mokaddes.ru2000@gmail.com>
2024-09-13 00:04:43 +0600

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        $user = new User();
+        $user->name = 'admin';
+        $user->email = 'admin@gmail.com';
+        $user->password = bcrypt('123456789');
+
+        $user->save();
+
+    }
+
+}
---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+<x-authenticated-Layout>
+
+        <x-slot name="title">About</x-slot>
+
+        <x-slot name="content">
+            <h1>About Us</h1>
+            <p>Learn more about our company.</p>
+        </x-slot>
+ </x-authenticated-Layout>
